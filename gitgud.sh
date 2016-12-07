246be43d37cf05da9a648df07602dc9257dd8236
icosa <icosahedron@hkcnews.com>
2016-12-07 14:25:55 +0800

    update to wrap code

--- gitgud.sh
+++ gitgud.sh
@@ -1,7 +1,11 @@
-if [ -d "$HOME/.git-gud" ]; then
-  rm -rf "$HOME/.git-gud"
-if
+main(){
+  if [ -d "$HOME/.git-gud" ]; then
+    rm -rf "$HOME/.git-gud"
+  if
 
-git clone https://github.com/icosa-hk/git-gud $HOME/.git-gud
-cd "$HOME/.git-gud"
-install.sh
+  git clone https://github.com/icosa-hk/git-gud $HOME/.git-gud
+  cd "$HOME/.git-gud"
+  install.sh
+}
+
+main
