4a1e89c4284713d7b439e8bc8bc5f28de5d98557
icosa <icosahedron@hkcnews.com>
2016-12-07 14:38:17 +0800

    fixed installer

--- gitgud.sh
+++ gitgud.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 main() {
   if [ -d "$HOME/.git-gud" ]; then
     rm -rf "$HOME/.git-gud"
-  if
+  fi
 
   git clone https://github.com/icosa-hk/git-gud $HOME/.git-gud
-  cd "$HOME/.git-gud"
-  install.sh
+  cd $HOME/.git-gud
+  bash ./install.sh
 }
 
 main
